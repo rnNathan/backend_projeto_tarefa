d28247e6cb71fcbf18a078687a9fadb71bd41df4
Nathan Rodrigues <nathanping73@gmail.com>
2024-07-01 10:30:37 -0300

    Criação para gerar um Usuário.

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import exception.TarefaException;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import model.dto.UsuarioDTO;
+import model.entity.Usuario;
+import service.LoginService;
+
+@Path("/login")
+public class LoginController {
+	
+	private LoginService loginService = new LoginService();
+	
+	@POST
+	@Path("/autenticar")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Usuario autenticar(UsuarioDTO usuarioTentandoSeAutenticar) throws TarefaException {
+		return loginService.autenticar(usuarioTentandoSeAutenticar);
+	}
+}
